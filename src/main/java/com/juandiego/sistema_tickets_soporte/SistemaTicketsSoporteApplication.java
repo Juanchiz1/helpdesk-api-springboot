281d81e6a972d4fea0d79127091da31ca35bcb4d
Juanchiz1 <juandinegrete2006@outlook.com>
2026-09-09 18:48:33 -0500

    chore: setup inicial del proyecto Spring Boot con PostgreSQL local funcionando

---
+++ src/main/java/com/juandiego/sistema_tickets_soporte/SistemaTicketsSoporteApplication.java
@@ -0,0 +1,13 @@
+package com.juandiego.sistema_tickets_soporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaTicketsSoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaTicketsSoporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juandiego/sistema_tickets_soporte/SistemaTicketsSoporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandiego.sistema_tickets_soporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTicketsSoporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
